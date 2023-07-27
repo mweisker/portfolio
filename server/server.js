1c882411af4f9d60e7bd8426be6d31b906d13fd0
Matthew Weisker <mweisker@live.com>
2023-07-27 11:33:57 -0700

    Got basic fullstack structure running

---
+++ server/server.js
@@ -0,0 +1,8 @@
+const express = require('express');
+const app = express();
+const path = require('path');
+
+app.use(express.static(path.join(__dirname, '../' ))); //serves the index.html
+app.listen(3000, () => {
+  console.log(`Server listening on port: 3000`)
+}); //listens on port 3000 -> http://localhost:3000/
---
+++ webpack.config.js
@@ -0,0 +1,46 @@
+const path = require('path'); 
+const HtmlWebpackPlugin = require('html-webpack-plugin'); 
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+
+
+module.exports = {
+  mode:  process.env.NODE_ENV, //sets the mode for webpack to determine how it optimizes the bundles
+  entry: './client/index.js',
+  output: {
+    filename: 'bundle.js',
+    path: path.resolve(__dirname, 'build'),
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js/, 
+        exclude: /node_modules/,
+        loader: 'babel-loader',
+        options: {
+          presets: ['@babel/preset-env', '@babel/preset-react']
+        }
+      },
+      {
+        test: /\.s[ac]ss/, //.sass or .scss 
+        use: [ 
+          "style-loader", 'css-loader', 'sass-loader'
+        ] 
+      },
+      {
+        test: /\.(png|jpg|gif)$/i,
+        type: 'asset/resource',
+      }
+      
+    ]
+  }, 
+  plugins: [ new HtmlWebpackPlugin({
+    title: 'Development',
+    template: 'index.html'
+   })
+], 
+  devServer: { 
+    proxy: {
+      '/': 'http://localhost:3000'
+    }
+  }
+};
