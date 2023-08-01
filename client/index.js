e656dc1fdb2c1dc956f76e6858f82e69af3c06aa
Matthew Weisker <mweisker@live.com>
2023-08-01 14:20:59 -0700

    Added a spinning opening page

--- client/index.js
+++ client/index.js
@@ -3,6 +3,8 @@ import { createRoot } from 'react-dom/client';
 import { BrowserRouter as Router } from 'react-router-dom';
 import App from './App.jsx';
 
+import './index.scss';
+
 createRoot(document.getElementById('root')).render(
   <Router>
       <App />
--- client/path/Home.jsx
+++ client/path/Home.jsx
@@ -16,6 +16,14 @@ export default function Home() {
       <Link to='About'>About Me</Link>
       <br />
       <Link to='Contact'>Contact Me</Link>
+
+      <div className='outer-circle circle center'>
+        <div className='link-circle circle'>Wee</div>
+        <div className='link-circle unique circle'>Unique</div>
+        <div className='link-circle circle'>Wee</div>
+
+      </div>
+      <div className='inner-circle circle center'></div>
     </div>
   )
 }
