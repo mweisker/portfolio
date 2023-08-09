fac4a593c14855711812843e46bcf73d4e97cf5b
Matthew Weisker <mweisker@live.com>
2023-08-08 21:21:06 -0700

    Added a profile picture

--- client/components/InnerCircleDefault.jsx
+++ client/components/InnerCircleDefault.jsx
@@ -2,12 +2,14 @@ import React, { useState, useEffect } from 'react';
 // import { useNavigate } from 'react-router-dom';
 import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom';
 
+import profile from '../images/profile.JPG';
 
 
 export default function innerCircleDefault() {
 
   return (
     <div className='inner-circle-component center circle default'>
+      <img src={profile} className='profile-photo' />
     </div>
   )
 }
