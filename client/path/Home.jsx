e28a1c09e060b7646b00421881c88e10cccdf110
Matthew Weisker <mweisker@live.com>
2023-08-09 12:11:42 -0700

    Set up icons with responsiveness in work page

--- client/path/Home.jsx
+++ client/path/Home.jsx
@@ -10,18 +10,26 @@ import Contact from '../components/InnerCircleContact.jsx';
 
 
 export default function Home() {
-  const [center, setCenter] = useState('default')
+  const [center, setCenter] = useState('default');
+  const [active, setActive] = useState({work: false, about: false, contact: false})
 
-  // const navigate = useNavigate();
-  console.log(center)
+  const handleMouseEnter = (activeCircle) => {
+    setCenter(activeCircle);
+    const activeCopy = {...active};
+    for (const circle in activeCopy) {
+      activeCopy[circle] = false;
+    }
+    activeCopy[activeCircle] = true;
+    setActive(activeCopy);
+  }
 
   return (
     <div className='main-div'>
 
       <div className='outer-circle circle center'>
-        <div className='link-circle about-circle circle' onMouseEnter={() => {setCenter('about')}}></div>
-        <div className='link-circle work-circle circle' onMouseEnter={() => {setCenter('work')}}></div>
-        <div className='link-circle contact-circle circle' onMouseEnter={() => {setCenter('contact')}}></div>
+        <div className={`link-circle about-circle circle spin ${active['about'] ? 'active' : ''}` } onMouseEnter={() => {handleMouseEnter('about')}}></div>
+        <div className={`link-circle work-circle circle spin ${active['work'] ? 'active' : ''}`} onMouseEnter={() => {handleMouseEnter('work')}}></div>
+        <div className={`link-circle contact-circle circle spin ${active['contact'] ? 'active' : ''}`} onMouseEnter={() => {handleMouseEnter('contact')}}></div>
 
       </div>
       <div className='inner-circle circle center background-circle'></div>
--- client/path/Work.jsx
+++ client/path/Work.jsx
@@ -1,15 +1,36 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
 
 // import './Work.scss'
 
 
 export default function Work() {
+  const [center, setCenter] = useState('home');
+  const [active, setActive] = useState({home: true, codesmith: false, obsidian: false, flyingCrow: false, procrastination: false})
+
+  const handleMouseEnter = (activeCircle) => {
+    setCenter(activeCircle);
+    const activeCopy = {...active};
+    for (const circle in activeCopy) {
+      activeCopy[circle] = false;
+    }
+    activeCopy[activeCircle] = true;
+    setActive(activeCopy);
+  }
 
   return (
     <div className='background background-work'>
       <h1>My Work</h1>
+      <div className='link-holder work-links'>
+        <div className={`link-circle circle codesmith ${active['codesmith'] ? 'active' : ''}`} onMouseEnter={() => {handleMouseEnter('codesmith')}}></div>
+        <div className={`link-circle circle obsidian ${active['obsidian'] ? 'active' : ''}`} onMouseEnter={() => {handleMouseEnter('obsidian')}}></div>
+        <div className={`link-circle circle flying-crow ${active['flyingCrow'] ? 'active' : ''}`} onMouseEnter={() => {handleMouseEnter('flyingCrow')}}></div>
+        <div className={`link-circle circle procrastination ${active['procrastination'] ? 'active' : ''}`} onMouseEnter={() => {handleMouseEnter('procrastination')}}></div>
+      </div>
       <Link to='/'>Home</Link>
+      <div className='link-holder home-link'>
+        <div className={`link-circle circle home ${active['home'] ? 'active' : ''}`} onMouseEnter={() => {handleMouseEnter('home')}}></div>
+      </div>
     </div>
   )
 }
